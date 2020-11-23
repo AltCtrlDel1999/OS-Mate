0e6c551f8ae80a68bb69481d93c29e9c98bc74f3
Ishaan-11 <ishaansharma1998@gmail.com>
2020-11-23 21:38:03 +0530

    Footer changes

--- osmate/src/Footer.js
+++ osmate/src/Footer.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Footer.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col} from 'react-bootstrap';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 function Footer() {
   const currentYear = new Date().getFullYear();
 
   return(
     <footer>
-      <p>Copyright ⓒ {currentYear}</p>
+      <Container>
+        <Row>
+          <Col>© {currentYear} OS-Mate</Col>
+          <Col>Get all Open Source Events and Hackathons under one roof</Col>
+          <Col><a href="https://github.com/AltCtrlDel1999/OS-Mate"><GitHubIcon style={{fill: "black"}}/></a></Col>
+        </Row>
+      </Container>
     </footer>
   );
 }
